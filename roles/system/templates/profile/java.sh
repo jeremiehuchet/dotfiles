d1a55ab9242e8e1350f63ea1df2a31eda4b58dac
Jeremie Huchet <jeremiehuchet@users.noreply.github.com>
2017-08-20 09:42:41 +0200

    java/maven env vars

---
+++ roles/system/templates/profile/java.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# {{ generated_tag }}
+
+JAVA_HOME=/opt/jdk
+export JAVA_HOME
+
+PATH=$JAVA_HOME/bin:$PATH
+export PATH
---
+++ roles/system/templates/profile/maven.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# {{ generated_tag }}
+
+M2_HOME=/opt/apache-maven
+export M2_HOME
+
+PATH=$M2_HOME/bin:$PATH
+export PATH
